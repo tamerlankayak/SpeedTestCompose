f645e706bc76f71238299e0abc17c2da9d0d105b
tamerlankayak <tamerlan_94@mail.ru>
2023-05-05 14:37:37 +0400

    created additional info functions

--- app/src/main/java/com/example/speedtest/SpeedTestScreen.kt
+++ app/src/main/java/com/example/speedtest/SpeedTestScreen.kt
@@ -1,14 +1,34 @@
 package com.example.speedtest
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.speedtest.ui.theme.ComposeSpeedTestTheme
+import com.example.speedtest.ui.theme.DarkColor
 import com.example.speedtest.ui.theme.DarkGradient
 
 @Composable
@@ -26,10 +46,99 @@ private fun SpeedTestScreen(state: UiState, onClick: () -> Unit) {
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .fillMaxSize()
-            .background(DarkGradient)
+            .background(DarkGradient),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Header()
+        SpeedIndicator(state = state, onClick = onClick)
+        AdditionalInfo(state.ping, state.maxSpeed)
+        NavigationView()
+    }
+}
+
+@Composable
+fun SpeedIndicator(state: UiState, onClick: () -> Unit) {
+    Box(
+        contentAlignment = Alignment.BottomCenter,
+        modifier = Modifier
+            .fillMaxWidth()
+            .aspectRatio(1f)
     ) {
 
     }
+}
+
+@Composable
+fun NavigationView() {
+    val items = listOf(
+        R.drawable.wifi,
+        R.drawable.person,
+        R.drawable.speed,
+        R.drawable.settings,
+    )
+    val selectedItem = 2
+
+    BottomNavigation(backgroundColor = DarkColor) {
+        items.mapIndexed { index, item ->
+            BottomNavigationItem(
+                selected = index == selectedItem,
+                onClick = { },
+                unselectedContentColor = MaterialTheme.colors.onSurface,
+                icon = {
+                    Icon(painterResource(id = item), null)
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun AdditionalInfo(ping: String, maxSpeed: String) {
+
+    @Composable
+    fun RowScope.InfoColumn(title: String, value: String) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.weight(1f)
+        ) {
+            Text(text = title)
+            Text(
+                text = value,
+                style = MaterialTheme.typography.body2,
+                modifier = Modifier.padding(vertical = 8.dp)
+            )
+
+        }
+    }
+
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .height(IntrinsicSize.Min)
+    ) {
+        InfoColumn(title = "PING", value = ping)
+        VerticalDivider()
+        InfoColumn(title = "MAX SPEED", value = maxSpeed)
+    }
+}
+
+@Composable
+fun VerticalDivider() {
+    Box(
+        modifier = Modifier
+            .fillMaxHeight()
+            .background(Color(0xFF414D66))
+            .width(1.dp)
+    )
+}
+
+@Composable
+fun Header() {
+    Text(
+        text = "SPEEDTEST",
+        modifier = Modifier.padding(top = 52.dp, bottom = 16.dp),
+        style = MaterialTheme.typography.h6
+    )
 }
 
 @Preview(showBackground = true)
