5281be1bb29372dd93be4197ef9609662ac1cf42
tamerlankayak <tamerlan_94@mail.ru>
2023-05-05 15:16:35 +0400

    canvas created

--- app/src/main/java/com/example/speedtest/SpeedTestScreen.kt
+++ app/src/main/java/com/example/speedtest/SpeedTestScreen.kt
@@ -1,5 +1,6 @@
 package com.example.speedtest
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -23,13 +24,24 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.speedtest.ui.theme.ComposeSpeedTestTheme
 import com.example.speedtest.ui.theme.DarkColor
 import com.example.speedtest.ui.theme.DarkGradient
+import com.example.speedtest.ui.theme.Green200
+import com.example.speedtest.ui.theme.Green500
+import com.example.speedtest.ui.theme.GreenGradient
+import com.example.speedtest.ui.theme.LightColor
+import kotlin.math.floor
 
 @Composable
 fun SpeedTestScreen() {
@@ -64,9 +76,91 @@ fun SpeedIndicator(state: UiState, onClick: () -> Unit) {
             .fillMaxWidth()
             .aspectRatio(1f)
     ) {
+        CircularSpeedIndicator(state.arcValue, 240f)
 
     }
 }
+
+@Composable
+fun CircularSpeedIndicator(value: Float, angle: Float) {
+    Canvas(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(40.dp)
+    ) {
+        drawLines(value, angle)
+        drawArcs(value, angle)
+    }
+}
+
+fun DrawScope.drawLines(progress: Float, maxValue: Float, numberOfLines: Int = 40) {
+    val oneRotation = maxValue / numberOfLines
+    val startValue = if (progress == 0f) 0 else floor(progress * numberOfLines).toInt() + 1
+
+    for (i in startValue..numberOfLines) {
+        rotate(i * oneRotation + (180 - maxValue) / 2) {
+            drawLine(
+                LightColor,
+                Offset(if (i % 5 == 0) 80f else 30f, size.height / 2),
+                Offset(0f, size.height / 2),
+                8f,
+                StrokeCap.Round
+            )
+        }
+
+    }
+}
+
+fun DrawScope.drawArcs(progress: Float, maxValue: Float) {
+    val startAngle = 270 - maxValue / 2
+    val sweepAngle = maxValue * progress
+
+    val topLeft = Offset(50f, 50f)
+    val size = Size(size.width - 100f, size.height - 100f)
+
+    fun drawBlur() {
+        for (i in 0..20) {
+            drawArc(
+                color = Green200.copy(alpha = i / 900f),
+                startAngle = startAngle,
+                sweepAngle = sweepAngle,
+                useCenter = false,
+                topLeft = topLeft,
+                size = size,
+                style = Stroke(width = 80f + (20 - i) * 20, cap = StrokeCap.Round)
+            )
+        }
+    }
+
+    fun drawStroke() {
+        drawArc(
+            color = Green500,
+            startAngle = startAngle,
+            sweepAngle = sweepAngle,
+            useCenter = false,
+            topLeft = topLeft,
+            size = size,
+            style = Stroke(width = 86f, cap = StrokeCap.Round)
+        )
+    }
+
+    fun drawGradient() {
+        drawArc(
+            brush = GreenGradient,
+            startAngle = startAngle,
+            sweepAngle = sweepAngle,
+            useCenter = false,
+            topLeft = topLeft,
+            size = size,
+            style = Stroke(width = 80f, cap = StrokeCap.Round)
+        )
+    }
+
+    drawBlur()
+    drawStroke()
+    drawGradient()
+}
+
 
 @Composable
 fun NavigationView() {
@@ -151,7 +245,7 @@ fun SpeedTestScreenPreview() {
                     speed = "120.5",
                     ping = "5 ms",
                     maxSpeed = "150.0 mbps",
-                    arcValue = 0.83f
+                    arcValue = 0.25f
                 )
             ) {
 
